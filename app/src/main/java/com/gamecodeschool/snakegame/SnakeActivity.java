9949f3dbb54255f784f6e61d3d1658f880bed19c
Taekjin Jung <nabaekjo2015@gmail.com>
2024-03-22 16:21:14 -0700

    test3 for branches

--- app/src/main/java/com/gamecodeschool/snakegame/SnakeActivity.java
+++ app/src/main/java/com/gamecodeschool/snakegame/SnakeActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.view.Display;
+
 //github_branches Testing
 public class SnakeActivity extends Activity {
 
